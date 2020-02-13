c642cefd0a2f45802e9a90bc60b1401772b4f40f
David Arinder <david.arinder@gmail.com>
2020-02-13 15:05:54 -0800

    setup production webpck build

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,11 @@
-const path = require('path');
+const path = require('path')
+// const ExtractTextPlugin = require('extract-text-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 module.exports = (env) => {
-  const isProduction = env === production
+  const isProduction = env === 'production'
+  // const CSSExtract = new ExtractTextPlugin('styles.css')
+  const CSSExtract = new MiniCssExtractPlugin({ filename: 'styles.css' })
 
   console.log('env', env)
   return {
@@ -17,14 +21,33 @@ module.exports = (env) => {
         exclude: /node_modules/
       }, {
         test: /\.s?css$/,
+        // use: CSSExtract.extract({
+        //   use: [
+        //     'css-loader',
+        //     'sass-loader'
+        //   ]
+        // })
         use: [
-          'style-loader',
-          'css-loader',
-          'sass-loader'
+          MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+            options: {
+              sourceMap: true
+            }
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              sourceMap: true
+            }
+          }
         ]
       }]
     },
-    devtool: isProduction ? 'source-map' : 'cheap-module-eval-source-map',
+    plugins: [
+      CSSExtract
+    ],
+    devtool: isProduction ? 'source-map' : 'inline-source-map',
     devServer: {
       contentBase: path.join(__dirname, 'public'),
       historyApiFallback: true // tells dev-server we're handling routing via clientside code, will run index.html if 404 gets returned
