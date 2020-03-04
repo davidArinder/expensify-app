4631e771f88e9f86cd551c8fc390a042449c0c12
David Arinder <david.arinder@gmail.com>
2020-03-04 13:41:14 -0800

    adding PublicRoute

---
+++ src/routers/PublicRoute.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { connect } from 'react-redux'
+import { Route, Redirect } from 'react-router-dom'
+
+export const PublicRoute = ({ 
+    isAuthenticated, 
+    component: Component,
+    ...rest // gives access to all the other props to use isAuthenticated and component by themselves  
+}) => (
+    <Route {...rest} component={(props) => (
+        isAuthenticated ? ( 
+            <Redirect to="/dashboard" /> // redirect to '/dashboard' is user authenticated
+        ) : (
+            <Component {...props} /> // redirect if user not authenticated
+        )
+    )} />
+)
+
+const mapStateToProps = (state) => ({
+    isAuthenticated: !!state.auth.uid
+})
+
+export default connect(mapStateToProps)(PublicRoute)
